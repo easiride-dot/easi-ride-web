41f4ba2ae354727f710b7f7cb1fd68b01f6fc428
Mohamed Kolleh Mansaray <mohamedkolleh20@gmail.com>
2026-07-08 11:35:18 +0000

    Fix: use local maplibre-gl.css import instead of node_modules path

--- src/components/MapDisplay.tsx
+++ src/components/MapDisplay.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import maplibregl from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
+import "../map/maplibre-gl.css";
 import { DriverLocation } from "@/hooks/useDriverLocation";
 import { Ride } from "@/context/RideContext";
 
